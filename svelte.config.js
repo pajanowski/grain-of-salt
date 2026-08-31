817361232faa5e02bb18e0a1e113b063dc9d8f0b
phill <philajan@pm.me>
2026-08-30 20:26:19 -0500

    add cloudflare to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		typescript: {
-			config: (config) => ({
-				...config,
-				include: [...config.include, '../drizzle.config.ts']
-			})
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    typescript: {
+      config: (config) => ({
+        ...config,
+        include: [...config.include, '../drizzle.config.ts']
+      })
+    }
+  }
 };
 
 export default config;
